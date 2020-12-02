69f6471e40a0ebad95a2d88dc91927058b781212
Konstantin <konstantin.kuznetsov@kiwitaxi.com>
2020-12-02 18:13:25 +0400

    Initial Commit

---
+++ HUD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HUD
+//
+//  Created by Konstantin on 02/12/2020.
+//  Copyright © 2020 Konstantin Kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
